ab8a677a8e437c97e9c285ad7a9a697787520372
ashwanikr07 <ashwanikrsharma777@gmail.com>
2024-09-16 23:04:04 +0530

    update and commit

--- Githubproject/src/com/ashu/GithubProject.java
+++ Githubproject/src/com/ashu/GithubProject.java
@@ -5,7 +5,8 @@ public class GithubProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ashwani kumar");
-		System.out.println("ashwani kumar");
+		System.out.println("ashwani sharma");
+		
 
 	}
 
