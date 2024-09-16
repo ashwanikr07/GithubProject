0d834b6d361098cfa9955bfbf666e72f73362789
ashwanikr07 <ashwanikrsharma777@gmail.com>
2024-09-16 22:54:19 +0530

    this code commit

---
+++ Githubproject/src/com/ashu/GithubProject.java
@@ -0,0 +1,11 @@
+package com.ashu;
+
+public class GithubProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+
+	}
+
+}
